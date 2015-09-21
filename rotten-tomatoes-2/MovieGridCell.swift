91a985518609794bc4e66253c200b281bbc104e4
Kevin Chen <kevin.chen@kchen-mb13r.corp.creditkarma.com>
2015-09-20 22:52:54 -0700

    Adds readme and images

---
+++ rotten-tomatoes-2/MovieGridCell.swift
@@ -0,0 +1,15 @@
+//
+//  MovieGridCell.swift
+//  rotten-tomatoes-2
+//
+//  Created by Kevin Chen on 9/20/15.
+//  Copyright © 2015 Kevin Chen. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+  @IBOutlet weak var gridPosterView: UIImageView!
+  @IBOutlet weak var gridTitleLabel: UILabel!
+}
+
